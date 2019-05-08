9d30af0d377cdc179feeb0e91590a02a1012b5b8
NateCar1 <49995335+NateCar1@users.noreply.github.com>
2019-05-08 14:20:56 -0500

    Add files via upload

---
+++ com/cctc/amatlock/test/utilities/Images.java
@@ -0,0 +1,16 @@
+package com.cctc.amatlock.test.utilities;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * This class will just hold all the image variables
+ * used in our game. It is a purely organizational
+ * tool.
+ */
+public class Images
+{
+
+    public static BufferedImage background;
+    public static BufferedImage barry;
+    public static BufferedImage donkey;
+}
---
+++ com/cctc/amatlock/test/utilities/ResourceLoader.java
@@ -0,0 +1,30 @@
+package com.cctc.amatlock.test.utilities;
+
+import java.io.IOException;
+
+public class ResourceLoader
+{
+    // Creates an instance of ImageLoader
+    // This is used to load each image
+    private static ImageLoader imageLoader = new ImageLoader();
+
+    /**
+     * This method will load and store every image
+     * we plan to use in our program. This is the
+     * only method we will change when we need to
+     * add more images.
+     */
+    public static void loadImages()
+    {
+
+        try
+        {
+            Images.barry = imageLoader.loadImage("barry.png");
+            Images.background = imageLoader.loadImage("background.png");
+            Images.donkey = imageLoader.loadImage("donkey.png");
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
